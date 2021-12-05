3a8d9a17953c5aafe8119bf5bb220ece62941c23
Vova_Reznik <reznikvova2222@gmail.com>
2021-12-06 01:06:35 +0200

    MENR

---
+++ client/src/components/LinkCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export const LinkCard = ({ link }) => {
+  return (
+    <>
+      <h2>Посилання</h2>
+
+      <p>
+        Ваше посилання:{' '}
+        <a href={link.to} target="_blank" rel="noopener noreferrer">
+          {link.to}
+        </a>
+      </p>
+      <p>
+        Звідки:{' '}
+        <a href={link.from} target="_blank" rel="noopener noreferrer">
+          {link.from}
+        </a>
+      </p>
+      <p>
+        Кількість кліків: <strong>{link.clicks}</strong>
+      </p>
+      <p>
+        Дата створення: <strong>{new Date(link.date).toLocaleDateString()}</strong>
+      </p>
+    </>
+  );
+};
